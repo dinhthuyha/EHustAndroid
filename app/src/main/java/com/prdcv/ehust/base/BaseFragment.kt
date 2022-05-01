f427dd0fc8d670934de58c4032d8a16afca3a6d4
dinhthuyha <dinhthuyhatgmb@gmail.com>
2022-05-01 20:14:19 +0700

    base fragemnt + main fragemnt + layout home

---
+++ app/src/main/java/com/prdcv/ehust/base/BaseFragment.kt
@@ -0,0 +1,4 @@
+package com.prdcv.ehust.base
+
+class BaseFragment {
+}
---
+++ app/src/main/java/com/prdcv/ehust/ui/sreach/SeachFragment.kt
@@ -0,0 +1,32 @@
+package com.prdcv.ehust.ui.sreach
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.prdcv.ehust.R
+
+class SeachFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SeachFragment()
+    }
+
+    private lateinit var viewModel: SeachViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.seach_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SeachViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
