8b157c05fdf8579d185a45013d59951c03ca7f0a
Le Viet Hai <4000418@prdcv.local>
2022-05-20 16:46:43 +0700

    add layout calendar

---
+++ app/src/main/java/com/prdcv/ehust/ui/schedule/ScheduleFragment.kt
@@ -0,0 +1,18 @@
+package com.prdcv.ehust.ui.schedule
+
+import android.view.LayoutInflater
+import com.prdcv.ehust.base.BaseFragmentWithBinding
+import com.prdcv.ehust.databinding.FragmentScheduleBinding
+
+
+class ScheduleFragment : BaseFragmentWithBinding<FragmentScheduleBinding>() {
+    override fun getViewBinding(inflater: LayoutInflater)= FragmentScheduleBinding.inflate(inflater).apply {
+
+    }
+
+    override fun init() {
+
+    }
+
+
+}
