5f38c972c95fd380c34f4e15111c7cac287ea2ca
hai98 <haile197@gmail.com>
2022-05-28 01:32:42 +0700

    add localDate & localTime gson deserializer

--- app/src/main/java/com/prdcv/ehust/di/NetworkModule.kt
+++ app/src/main/java/com/prdcv/ehust/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.prdcv.ehust.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
 import com.prdcv.ehust.network.EHustClient
 import com.prdcv.ehust.network.EHustService
 import com.prdcv.ehust.utils.SharedPreferencesKey.EHUST
@@ -12,17 +13,31 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private val localTimeDeserializer = JsonDeserializer { json, _, _ ->
+        json?.let {
+            LocalTime.parse(it.asString, DateTimeFormatter.ofPattern("HH:mm"))
+        }
+    }
+
+    private val localDateDeserializer = JsonDeserializer { json, _, _ ->
+        json?.let {
+            LocalDate.parse(it.asString, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        }
+    }
 
     @Provides
     fun provideRetrofit(
@@ -39,8 +54,13 @@ object NetworkModule {
             .baseUrl("http://104.215.150.77/api/" )
             .addConverterFactory(
                 GsonConverterFactory.create(
-                    GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss").setLenient().create()
+                    GsonBuilder()
+                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                        .setLenient()
+                        .registerTypeAdapter(LocalTime::class.java, localTimeDeserializer)
+                        .registerTypeAdapter(LocalDate::class.java, localDateDeserializer)
+                        .create()
                 )
             )
                if (!token.isNullOrEmpty())
