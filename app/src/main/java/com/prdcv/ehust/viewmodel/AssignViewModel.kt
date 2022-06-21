a5fe3e83fb999ba1941c4ad27351f12172b09eda
hai98 <haile197@gmail.com>
2022-06-22 02:16:31 +0700

    refactor AssignScreen.kt

--- app/src/main/java/com/prdcv/ehust/viewmodel/AssignViewModel.kt
+++ app/src/main/java/com/prdcv/ehust/viewmodel/AssignViewModel.kt
@@ -1,5 +1,10 @@
 package com.prdcv.ehust.viewmodel
 
+import android.util.Log
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.prdcv.ehust.common.State
@@ -10,12 +15,26 @@ import com.prdcv.ehust.repo.SubjectRepository
 import com.prdcv.ehust.repo.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
 import javax.inject.Inject
+
+data class AssignScreenState(
+    val subjects: List<Subject> = emptyList(),
+    val students: List<User> = emptyList(),
+    val teachers: List<User> = emptyList(),
+    val selectedSubject: Subject? = null,
+    val selectedStudent: User? = null,
+    val selectedTeacher: User? = null,
+    val submitButtonEnabled: Boolean = true
+) {
+    fun isAllSelected(): Boolean {
+        return selectedSubject
+            ?.let { selectedStudent }
+            ?.let { selectedTeacher }
+            ?.let { true } ?: false
+    }
+}
 
 @HiltViewModel
 class AssignViewModel @Inject constructor(
@@ -23,23 +42,9 @@ class AssignViewModel @Inject constructor(
     private val subjectRepository: SubjectRepository
 ) : ViewModel() {
 
-
-    private var _projectsState: MutableStateFlow<State<List<Subject>>> =
-        MutableStateFlow(State.Loading)
-    val projectsState: StateFlow<State<List<Subject>>> get() = _projectsState
-
-    private var _studentsState: MutableStateFlow<State<List<User>>> =
-        MutableStateFlow(State.Loading)
-    val studentsState: StateFlow<State<List<User>>> get() = _studentsState
-
-    private var _teachersState: MutableStateFlow<State<List<User>>> =
-        MutableStateFlow(State.Loading)
-    val teachersState: StateFlow<State<List<User>>> get() = _teachersState
-
-    private var _assignState: MutableStateFlow<State<ResponseBody>> =
-        MutableStateFlow(State.Loading)
-    val assignState: StateFlow<State<ResponseBody>> get() = _assignState
-
+    val snackbarHostState: SnackbarHostState = SnackbarHostState()
+    var uiState by mutableStateOf(AssignScreenState())
+        private set
 
     /**
      * admin get ds project cua toan truong
@@ -47,31 +52,75 @@ class AssignViewModel @Inject constructor(
     fun getAllProjectCurrentSemester() {
         viewModelScope.launch(Dispatchers.IO) {
             subjectRepository.getAllProjectCurrentSemester().collect {
-                _projectsState.emit(it)
-            }
-        }
-    }
-
-    fun getAllUserInClass(nameCourse: String, role: Role) {
-        viewModelScope.launch(Dispatchers.IO) {
-            userRepository.getAllUserInProject(nameCourse, role).collect {
-                when(role) {
-                    Role.ROLE_TEACHER -> _teachersState.emit(it)
-                    Role.ROLE_STUDENT -> _studentsState.emit(it)
+                when (val state = it) {
+                    is State.Success -> {
+                        uiState = uiState.copy(subjects = state.data)
+                    }
                     else -> {}
                 }
             }
         }
     }
 
-    fun assign(
+    private fun getAllUserInClass(nameCourse: String, role: Role) {
+        viewModelScope.launch(Dispatchers.IO) {
+            userRepository.getAllUserInProject(nameCourse, role).collect {
+                when (val state = it) {
+                    is State.Success -> {
+                        uiState = when (role) {
+                            Role.ROLE_TEACHER -> uiState.copy(teachers = state.data)
+                            Role.ROLE_STUDENT -> uiState.copy(students = state.data)
+                            else -> uiState
+                        }
+                    }
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    fun onProjectSelected(project: Subject) {
+        uiState =
+            uiState.copy(selectedSubject = project, selectedStudent = null, selectedTeacher = null)
+        getAllUserInClass(project.name, Role.ROLE_TEACHER)
+        getAllUserInClass(project.name, Role.ROLE_STUDENT)
+    }
+
+    fun onTeacherSelected(user: User) {
+        uiState = uiState.copy(selectedTeacher = user)
+    }
+
+    fun onStudentSelected(user: User) {
+        uiState = uiState.copy(selectedStudent = user)
+    }
+
+    fun onSubmit() {
+        Log.d("AssignVM", "onSubmit: clicked")
+        if (uiState.isAllSelected()) {
+            uiState = uiState.copy(submitButtonEnabled = false)
+            assign(
+                uiState.selectedStudent!!.id,
+                uiState.selectedTeacher!!.id,
+                uiState.selectedSubject!!.name
+            )
+        }
+    }
+
+    private fun assign(
         idStudent: Int,
         idTeacher: Int,
         nameProject: String
     ) {
         viewModelScope.launch {
-            userRepository.assignProjectInstructions(idStudent, idTeacher, nameProject).collect{
-                _assignState.emit(it)
+            userRepository.assignProjectInstructions(idStudent, idTeacher, nameProject).collect {
+                when (val state = it) {
+                    is State.Error -> snackbarHostState.showSnackbar("Error: ${state.exception}")
+                    is State.Success -> {snackbarHostState.showSnackbar("Success")
+                    uiState = AssignScreenState(subjects = uiState.subjects)
+                    }
+                    else -> return@collect
+                }
+                uiState = uiState.copy(submitButtonEnabled = true)
             }
         }
     }
