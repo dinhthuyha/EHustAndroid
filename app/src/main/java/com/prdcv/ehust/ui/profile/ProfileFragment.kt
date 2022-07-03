2f006c706be1a58db8a48452d167680bb61cece4
dinhthuyha <dinhthuyhatgmb@gmail.com>
2022-07-04 01:05:14 +0700

    logout and delete token

--- app/src/main/java/com/prdcv/ehust/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/prdcv/ehust/ui/profile/ProfileFragment.kt
@@ -1,21 +1,26 @@
 package com.prdcv.ehust.ui.profile
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.prdcv.ehust.base.BaseFragment
 import com.prdcv.ehust.model.User
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProfileFragment : BaseFragment() {
     private var user: User? = null
     var args: ProfileFragmentArgs? = null
     private val TAG = "ProfileFragment"
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     companion object {
         fun newInstance() = ProfileFragment()
@@ -34,7 +39,7 @@ class ProfileFragment : BaseFragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                ProfileCard(user = user)
+                ProfileCard(user = user,findNavController(), sharedPreferences)
             }
         }
     }
