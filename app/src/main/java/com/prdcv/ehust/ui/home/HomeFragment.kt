de12386df45e747499a8b9ba78054182e7096bbf
dinhthuyha <dinhthuyhatgmb@gmail.com>
2022-07-13 08:20:35 +0700

    add home screen logic

--- app/src/main/java/com/prdcv/ehust/ui/home/HomeFragment.kt
+++ app/src/main/java/com/prdcv/ehust/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,20 +18,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.akexorcist.snaptimepicker.SnapTimePickerDialog
+import com.prdcv.ehust.base.BaseFragment
 import com.prdcv.ehust.base.BaseFragmentWithBinding
 import com.prdcv.ehust.common.State
 import com.prdcv.ehust.databinding.HomeFragmentBinding
 import com.prdcv.ehust.extension.toLocalDate
 import com.prdcv.ehust.extension.toLocalTime
 import com.prdcv.ehust.model.Meeting
+import com.prdcv.ehust.ui.home.HomeScreen
 import com.prdcv.ehust.ui.home.MeetingToDayAdapter
 import com.prdcv.ehust.ui.home.ScheduleTodayAdapter
 import com.prdcv.ehust.ui.main.MainFragmentDirections
 import com.prdcv.ehust.ui.task.TaskRow
+import com.prdcv.ehust.ui.task.TaskScreenPreview
 import com.prdcv.ehust.ui.task.detail.TaskDetailArgs
 import com.prdcv.ehust.viewmodel.TaskViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,7 +48,7 @@ import java.util.*
 
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragmentWithBinding<HomeFragmentBinding>() {
+class HomeFragment : BaseFragment() {
     private val scheduleTodayAdapter = ScheduleTodayAdapter()
     private val meetingTodayAdapter = MeetingToDayAdapter()
     val taskViewModel: TaskViewModel by viewModels()
@@ -142,100 +149,102 @@ class HomeFragment : BaseFragmentWithBinding<HomeFragmentBinding>() {
         textView.text = String.format(getString(R.string.selected_time_format, hour, minute))
     }
 
-    override fun getViewBinding(inflater: LayoutInflater): HomeFragmentBinding =
-        HomeFragmentBinding.inflate(inflater).apply {
-            user = shareViewModel.user
-            rvScheduleToday.adapter = scheduleTodayAdapter
-            rvMeetingToday.adapter = meetingTodayAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val taskViewModel: TaskViewModel = hiltViewModel()
+                HomeScreen(shareViewModel.user?.roleId!!, findNavController(),taskViewModel, callback = {showDialog()})
+            }
         }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         taskViewModel.findAllTaskWillExpire()
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
-    override fun init() {
-        shareViewModel.meetings.observe(viewLifecycleOwner) {
-            when (it) {
-                is State.Success -> {
-                  //  meetingTodayAdapter.setItems(it.data)
-                }
-                else -> {}
-            }
-        }
-        binding.viewStudent.composeTask.setContent {
-            val taskViewModel: TaskViewModel = hiltViewModel()
-            LaunchedEffect(key1 = Unit) {
 
-            }
-            val uiState = taskViewModel.uiState
-            Column(modifier = Modifier.wrapContentHeight()) {
-                uiState.filteredTaskList.forEach { item ->
-                    TaskRow(
-                        data = item,
-                        modifier = Modifier
-                            .clickable {
-                                findNavController().navigate(
-                                    MainFragmentDirections.actionMainFragmentToDetailTaskFragment(
-                                        TaskDetailArgs(idTask = item.id)
-                                    )
-                                )
-                            }
-                    )
-                }
-            }
-        }
-        binding.rvScheduleToday.setOnClickListener {
-
-        }
-        binding.viewStudent.cdClassStudent.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_studentsFragment)
-        }
-        binding.viewStudent.cdProject.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_projectGraduateFragment)
-        }
-        binding.tbHome.iconRightId.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_newsFragment)
-        }
-        binding.viewStudent.cdSchedule.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_scheduleFragment)
-        }
-        shareViewModel.schedulesState.observe(viewLifecycleOwner) {
-            when (it) {
-                is State.Loading -> {
-                }
-                is State.Success -> {
-                    shareViewModel.schedules = it.data
-                    if (shareViewModel.getScheduleToday(it.data).isEmpty()) {
-                        binding.txtNoScheduler.apply {
-                            visibility = View.VISIBLE
-                            text = "Hôm nay bạn không có lịch học trên trường"
-                        }
-                    }
-                    scheduleTodayAdapter.setItems(shareViewModel.getScheduleToday(it.data))
-                    Log.d(TAG, "success: ${it.data.size}")
-
-                }
-                is State.Error -> {
-                    Log.d(TAG, "error: ${it.exception} ")
-                }
-            }
-        }
-
-        /**
-         * teacher event
-         */
+//    override fun init() {
+//        shareViewModel.meetings.observe(viewLifecycleOwner) {
+//            when (it) {
+//                is State.Success -> {
+//                  //  meetingTodayAdapter.setItems(it.data)
+//                }
+//                else -> {}
+//            }
+//        }
+//        binding.viewStudent.composeTask.setContent {
+//            val taskViewModel: TaskViewModel = hiltViewModel()
+//            LaunchedEffect(key1 = Unit) {
+//
+//            }
+//            val uiState = taskViewModel.uiState
+//            Column(modifier = Modifier.wrapContentHeight()) {
+//                uiState.filteredTaskList.forEach { item ->
+//                    TaskRow(
+//                        data = item,
+//                        modifier = Modifier
+//                            .clickable {
+//                                findNavController().navigate(
+//                                    MainFragmentDirections.actionMainFragmentToDetailTaskFragment(
+//                                        TaskDetailArgs(idTask = item.id)
+//                                    )
+//                                )
+//                            }
+//                    )
+//                }
+//            }
+//        }
+//        binding.rvScheduleToday.setOnClickListener {
+//
+//        }
 
 
-        binding.viewTeacher.cdNews.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_newsFragment)
-        }
-        binding.viewTeacher.cdProject.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_projectGraduateFragment)
-        }
-        binding.imgAdd.setOnClickListener { showDialog() }
-    }
+//        binding.tbHome.iconRightId.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_newsFragment)
+//        }
+
+//        shareViewModel.schedulesState.observe(viewLifecycleOwner) {
+//            when (it) {
+//                is State.Loading -> {
+//                }
+//                is State.Success -> {
+//                    shareViewModel.schedules = it.data
+//                    if (shareViewModel.getScheduleToday(it.data).isEmpty()) {
+//                        binding.txtNoScheduler.apply {
+//                            visibility = View.VISIBLE
+//                            text = "Hôm nay bạn không có lịch học trên trường"
+//                        }
+//                    }
+//                    scheduleTodayAdapter.setItems(shareViewModel.getScheduleToday(it.data))
+//                    Log.d(TAG, "success: ${it.data.size}")
+//
+//                }
+//                is State.Error -> {
+//                    Log.d(TAG, "error: ${it.exception} ")
+//                }
+//            }
+//        }
+//
+//        /**
+//         * teacher event
+//         */
+//
+//
+//        binding.viewTeacher.cdNews.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_newsFragment)
+//        }
+//        binding.viewTeacher.cdProject.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_projectGraduateFragment)
+//        }
+//        binding.imgAdd.setOnClickListener { showDialog() }
+//    }
 
 
 }
--- app/src/main/java/com/prdcv/ehust/ui/home/HomeScreen.kt
+++ app/src/main/java/com/prdcv/ehust/ui/home/HomeScreen.kt
@@ -11,51 +11,111 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.hadt.ehust.model.TopicStatus
+import androidx.navigation.NavController
 import com.prdcv.ehust.R
+import com.prdcv.ehust.model.Role
 import com.prdcv.ehust.ui.compose.Button
 import com.prdcv.ehust.ui.compose.DefaultTheme
-import com.prdcv.ehust.ui.profile.ToolBar
-import com.prdcv.ehust.ui.projects.topic.TopicsFragmentDirections
+import com.prdcv.ehust.ui.main.MainFragmentDirections
+import com.prdcv.ehust.ui.task.TaskRow
+import com.prdcv.ehust.ui.task.detail.TaskDetailArgs
+import com.prdcv.ehust.viewmodel.TaskViewModel
 import org.bouncycastle.asn1.x500.style.RFC4519Style.title
 
-@Preview(showBackground = true)
+
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    role: Role = Role.ROLE_STUDENT,
+    navController: NavController,
+    taskViewModel: TaskViewModel,
+    callback: () -> Unit
+) {
+    val uiState = taskViewModel.uiState
     DefaultTheme() {
-        Scaffold(topBar = { ToolBar(title = "Trang chủ")}) {
-            Column {
-                RowScheduleToday()
+        Scaffold(topBar = { ToolBar(title = "Trang chủ", nav = navController) }) {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                RowScheduleToday(role = role, callback = callback)
                 Spacer(modifier = Modifier.height(12.dp))
-                RowStudent()
-                RowTeacher()
+                uiState.filteredTaskList.forEach { item ->
+                    TaskRow(
+                        data = item,
+                        modifier = Modifier
+                            .padding(start = 10.dp, end = 10.dp, bottom = 5.dp)
+                            .clickable {
+                                navController.navigate(
+                                    MainFragmentDirections.actionMainFragmentToDetailTaskFragment(
+                                        TaskDetailArgs(idTask = item.id)
+                                    )
+                                )
+                            }
+                    )
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                when (role) {
+                    Role.ROLE_STUDENT -> {
+                        RowStudent(nav = navController)
+                    }
+                    Role.ROLE_TEACHER -> {
+                        RowTeacher(nav = navController)
+                    }
+                    else -> {}
+                }
+
+
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun RowScheduleToday() {
+fun ToolBar(title: String, nav: NavController) {
+    TopAppBar(backgroundColor = colorResource(id = R.color.text_color),
+        title = {
+            Text(
+                text = title,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 21.sp,
+            )
+        },
+        actions = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_notifications),
+                contentDescription = "",
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 10.dp)
+                    .clickable { nav.navigate(R.id.action_mainFragment_to_newsFragment) }
+            )
+        })
+}
+
+
+@Composable
+fun RowScheduleToday(role: Role = Role.ROLE_TEACHER, callback: () -> Unit) {
+    val alpha = if (role == Role.ROLE_TEACHER) 1f else 0f
     Card(
         elevation = 4.dp,
         shape = MaterialTheme.shapes.medium,
@@ -68,31 +128,40 @@ fun RowScheduleToday() {
                 shape = RectangleShape
             )
 
-    ){
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(24.dp)
-                , horizontalAlignment = Alignment.Start
+                .padding(24.dp), horizontalAlignment = Alignment.Start
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(4.dp)
             ) {
-                Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.weight(2f)) {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.weight(3f),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_calendar2),
                         contentDescription = ""
                     )
+                    Spacer(modifier = Modifier.width(5.dp))
                     Text(text = "Lịch hôm nay", fontWeight = FontWeight.Bold)
                 }
-                Row(horizontalArrangement = Arrangement.End, modifier = Modifier.weight(3f)) {
-                    Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "")
-                }
+
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier
+                        .weight(3f)
+                        .alpha(alpha)) {
+                Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "", modifier = Modifier.clickable { callback.invoke() })
+            }
 
             }
-            Divider(color = Color.Black, thickness = 1.dp)
+            Divider(color = Color.Black, thickness = 1.dp, modifier = Modifier.padding(start = 5.dp))
             RowItemSchedule()
             RowItemSchedule()
             RowItemSchedule()
@@ -102,15 +171,19 @@ fun RowScheduleToday() {
 
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun RowTeacher() {
+fun RowTeacher(nav: NavController) {
     Column() {
-        Row(title = "Đồ Án", sub = "Thông tin chi tiết đồ án", idIcon = R.drawable.ic_project)
+        Row(title = "Đồ Án",
+            sub = "Thông tin chi tiết đồ án",
+            idIcon = R.drawable.ic_project,
+            callback = { nav.navigate(R.id.action_mainFragment_to_projectGraduateFragment) })
         Row(
             title = "Thông báo đồ án",
             sub = "Thông báo chi tiết đồ án",
-            idIcon = R.drawable.ic_class
+            idIcon = R.drawable.ic_class,
+            callback = { nav.navigate(R.id.action_mainFragment_to_newsFragment) }
         )
     }
 }
@@ -132,28 +205,41 @@ fun RowItemSchedule() {
     }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun RowStudent() {
+fun RowStudent(nav: NavController) {
     Column() {
         Row(
             title = "Thời khoá biểu",
             sub = "Tra cứu thời khoá biểu",
-            idIcon = R.drawable.ic_calendar2
+            idIcon = R.drawable.ic_calendar2,
+            callback = { nav.navigate(R.id.action_mainFragment_to_scheduleFragment) }
+
         )
-        Row(title = "Đồ Án", sub = "Thông tin chi tiết đồ án", idIcon = R.drawable.ic_project)
-        Row(title = "Lớp sinh viên", sub = "Thông tin lớp sinh viên", idIcon = R.drawable.ic_class)
+        Row(
+            title = "Đồ Án",
+            sub = "Thông tin chi tiết đồ án",
+            idIcon = R.drawable.ic_project,
+            callback = { nav.navigate(R.id.action_mainFragment_to_projectGraduateFragment) }
+        )
+        Row(
+            title = "Lớp sinh viên",
+            sub = "Thông tin lớp sinh viên",
+            idIcon = R.drawable.ic_class,
+            callback = { nav.navigate(R.id.action_mainFragment_to_studentsFragment) }
+        )
     }
 
 
 }
 
-@Preview(showBackground = true)
+
 @Composable
 fun Row(
     title: String = "Đồ án",
     sub: String = "Thông tin chi tiết đo án",
-    idIcon: Int = R.drawable.ic_project
+    idIcon: Int = R.drawable.ic_project,
+    callback: () -> Unit
 ) {
     Card(
         elevation = 4.dp,
@@ -166,6 +252,7 @@ fun Row(
                 spotColor = Button,
                 shape = RectangleShape
             )
+            .clickable { callback.invoke() }
 
     ) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(12.dp)) {
