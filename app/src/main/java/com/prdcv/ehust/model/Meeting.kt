81805ee9fe660dfe97f108c6052432007b8335e4
dinhthuyha <dinhthuyhatgmb@gmail.com>
2022-07-24 07:32:55 +0700

    sua lai ten truong model meeting

--- app/src/main/java/com/prdcv/ehust/model/Meeting.kt
+++ app/src/main/java/com/prdcv/ehust/model/Meeting.kt
@@ -6,11 +6,11 @@ import java.time.LocalTime
 
 data class Meeting (
     val id: Int? = null,
-    @SerializedName( "id_user_teacher")
+    @SerializedName( "id_teacher")
     val idUserTeacher: Int,
-    @SerializedName( "id_user_student")
+    @SerializedName( "id_student")
     val idUserStudent: Int? = null,
-    @SerializedName( "name_user_student")
+    @SerializedName( "name_student")
     val nameStudent: String,
     val title: String,
     val date: LocalDate,
